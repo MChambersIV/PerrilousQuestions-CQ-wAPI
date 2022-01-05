8617bb39ac09bd730435d95a6414797ea19259f2
MChambersIV <chambersm025@gmail.com>
2022-01-04 23:04:02 -0800

    added seconds decrement function to script.js renamed asset folder to be fully lowercase

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,27 @@
-var question = document.getElementById("#qquestion")
-var answer = document.getElementById(".qanswer")
-var start = document.getElementById("#qstart")
+var question = document.getElementById('qquestion');
+var answer = document.getElementById('qanswer');
+var start = document.getElementById('qstart');
+var timer = document.getElementById('qtimer');
 
 var qlist = [
     "Of the three major coding languages, which is the most integral part of a webpage",
     "What is Javascript's file extension?",
     "You notice an error on a webpage, you open the developer tools of your browser to find the error in the code, where do you look first?",
     "Which file type is not typically associated with a webpage?",
-]
+];
 
 var qans1 = [
     "HTML",
     "CSS",
     "Javascript",
-]
+];
 
 var qans2 = [
     ".jar",
     ".jss",
     ".js",
     ".java"
-]
+];
 
 var qans3 = [
     "Storage",
@@ -28,7 +29,7 @@ var qans3 = [
     "Application",
     "Console",
 
-]
+];
 
 var qans4 = [
     ".html",
@@ -37,14 +38,26 @@ var qans4 = [
     ".css",
     
 
-]
+];
 
 
-start.addEventerLister("click", runclock())
+function runClock() {
+    var time = 30;
+    var timeCount = setInterval(function () {
 
+        if (timeCount > 1) {
+            timer.textContent = time + ' seconds left!';
+            time--;
+        } else if (timeCount === 1) {
+            timer.textContent = time + ' second left!'
+            time--;
+        } else {
+            timer.textContent = 'TIMES UP!';
+            clearInterval(timeCount);
+            
+        }
+    }, 1000);
 
-function runclock() {
+};
 
-
-
-}
+start.addEventListener("click", runClock)
