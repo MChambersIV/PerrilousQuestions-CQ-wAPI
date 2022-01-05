aa708e5edb02675473ec4cd11008124bd58c9710
MChambersIV <chambersm025@gmail.com>
2022-01-04 17:59:07 -0800

    added quiz question and answer arrays

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,50 @@
-var auestion = document.getElementById("#qquestion")
+var question = document.getElementById("#qquestion")
 var answer = document.getElementById(".qanswer")
 var start = document.getElementById("#qstart")
 
+var qlist = [
+    "Of the three major coding languages, which is the most integral part of a webpage",
+    "What is Javascript's file extension?",
+    "You notice an error on a webpage, you open the developer tools of your browser to find the error in the code, where do you look first?",
+    "Which file type is not typically associated with a webpage?",
+]
 
-start.addEventerLister("click", runclock)
+var qans1 = [
+    "HTML",
+    "CSS",
+    "Javascript",
+]
+
+var qans2 = [
+    ".jar",
+    ".jss",
+    ".js",
+    ".java"
+]
+
+var qans3 = [
+    "Storage",
+    "Memory",
+    "Application",
+    "Console",
+
+]
+
+var qans4 = [
+    ".html",
+    ".mp3",
+    ".js",
+    ".css",
+    
+
+]
+
+
+start.addEventerLister("click", runclock())
+
 
 function runclock() {
-    setInterval
-}
 
-function answer() {
-    if (time === 0 && );
-    
+
+
 }
